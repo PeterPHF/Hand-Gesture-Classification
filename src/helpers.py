ebacf38aa05bb5854d4e36e06960edd0d0c9ae82
Peter Hany <peter.hani03@gmail.com>
2026-02-23 18:55:22 +0200

    implement show_metrics

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,5 @@
-from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 def calculate_metrics(y_true, y_pred, average='weighted'):
     """
@@ -19,3 +20,16 @@ def calculate_metrics(y_true, y_pred, average='weighted'):
         "recall": recall_score(y_true, y_pred, average=average)
     }
     return metrics
+
+def show_metrics(model, x_test_prep, y_test):
+    y_test_pred = model.predict(x_test_prep)
+    metrics = {
+            "accuracy": accuracy_score(y_test, y_test_pred),
+            "f1_score": f1_score(y_test, y_test_pred, average='weighted'),
+            "precision": precision_score(y_test, y_test_pred, average='weighted'),
+            "recall": recall_score(y_test, y_test_pred, average='weighted')
+        }
+    print(metrics)
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ConfusionMatrixDisplay.from_predictions(y_test, y_test_pred, ax=ax, xticks_rotation=90)
+    ax.set_title(f"Confusion Matrix - {model.__class__.__name__}")
